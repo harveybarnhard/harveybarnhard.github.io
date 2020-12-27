2aa2af1f101030c31093446e1cc61a32148d1f34
Harvey Barnhard <harveybarnhard@gmail.com>
2020-12-27 18:59:25 -0500

    Adding blog links (without css)

--- public/js/route-config.js
+++ public/js/route-config.js
@@ -3,20 +3,23 @@ var app = angular.module('sampleApp',['ngRoute']);
 app.config(function ($routeProvider){
   $routeProvider
   .when('/',{
-    templateUrl:'home.html',
+    templateUrl: 'home.html',
   })
   .when('/endurance',{
-    templateUrl:'endurance.html',
+    templateUrl: 'endurance.html',
     controller: 'enduranceCtrl'
   })
   .when('/blog',{
-    templateUrl:'blog.html',
+    templateUrl: 'blog.html',
+  })
+  .when('/blog/evaluating-prediction-error',{
+    templateUrl: 'posts/evaluating-prediction-error.html',
   })
   .when('/projects',{
-    templateUrl:'projects.html',
+    templateUrl: 'projects.html',
   })
   .when('/about',{
-    templateUrl:'about.html',
+    templateUrl: 'about.html',
   })
   .otherwise({ redirectTo:'/'});
 })
