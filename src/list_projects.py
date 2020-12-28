c749c1eb66329edef703cc64a02f34f5d07f02c2
Harvey Barnhard <harveybarnhard@gmail.com>
2020-12-28 18:15:24 -0500

    Continued work on automating projects page

--- src/list_projects.py
+++ src/list_projects.py
@@ -1,18 +1,58 @@
 import dominate
+import pandas as pd
 from dominate.tags import *
+from dominate.util import raw
 
 # File to write
-projectsHTML = open("../public/projects.html", "w")
+outputHTML   = open("../public/projects_test.html", "w")
 
-# Create outer div and header
-list = div(cls="main-content")
-list += h1("Projects")
+# Create sidebar
+sidebar = raw(
+  """
+  <div class="sidebar">
+    <button class="barlink" style="font-size:20px; cursor: auto;">Categories</button>
+    <button class="barlink-item bar-active" onclick="openLink(event, 'projects-programming')">Programming</button>
+    <button class="barlink-item" onclick="openLink(event, 'projects-papers')">Papers</button>
+    <button class="barlink-item" onclick="openLink(event, 'projects-other')">Miscellany</button>
+  </div>
+  """
+)
 
-# Iterate through list of projects and create the div
+# Create outer div
+outHTML = div()
+main = div(cls="main-content")
+
+# Create Programming div
+file_prog = pd.read_csv("../posts/programming.csv")
+div_prog = div(id="projects-programming", cls="category fade-anim")
+for index, row in file_prog.iterrows():
+    div_item = div(cls="post")
+    div_item += div(row['Name'], cls="post-title")
+    div_item += a("[GitHub]", href=row['GitHub Link'])
+    div_item += div(row['Author'], cls="post-author")
+    div_item += div(row['Date'], cls="post-date")
+    div_prog += div_item
+    print(div_item)
+
+# Create Papers div
+div_paper = div(id="projects-papers", cls="category fade-anim", style="display:none")
 for item in range(4):
-    list += div(item, cls="hello")
-print(list)
+    div_paper += div(item, cls="hello")
+# Create Miscellany div
+div_misc = div(id="projects-other", cls="category fade-anim", style="display:none")
+for item in range(4):
+    div_misc += div(item, cls="hello")
+
+# Add categories to main div
+main += div_prog
+main += div_paper
+main += div_misc
+
+# Append sidebar and main content
+outHTML += sidebar
+outHTML += main
+print(outHTML)
 
 # Write the file
-projectsHTML.write(list.render())
-projectsHTML.close()
+outputHTML.write(outHTML.render())
+outputHTML.close()
