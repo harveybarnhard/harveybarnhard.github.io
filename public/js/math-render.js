28fe1156889da6aaa7a758ee6ff5fac685ba156f
Harvey Barnhard <harveybarnhard@gmail.com>
2020-12-29 15:36:08 -0500

    More updates to blog style

--- public/js/math-render.js
+++ public/js/math-render.js
@@ -1,3 +1,8 @@
 app.controller('mathRender', function() {
+	// Load LaTeX rendering
 	MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+	// Load code highlighting
+	document.querySelectorAll('pre code').forEach((block) => {
+		hljs.highlightBlock(block);
+	});
 })
